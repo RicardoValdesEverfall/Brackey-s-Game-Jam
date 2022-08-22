9fe16843fceb923f58edec4af0e029d5d45d207f
GamerWarlord <105244509+GamerWarlord@users.noreply.github.com>
2022-08-22 14:08:28 -0400

    Magic Sound Square
    Its magical

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_A_BLIZZARD_AMBIENT = 3566274169U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
