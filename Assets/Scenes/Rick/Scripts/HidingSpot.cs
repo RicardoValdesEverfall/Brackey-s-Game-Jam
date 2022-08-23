52a9046280981e3a9f58a66e4ea158acf93c8d40
Ricardo Valdes <ricardovaldes@everfall-games.com>
2022-08-23 18:36:09 -0400

    Started Hiding mechanic

---
+++ Assets/Scenes/Rick/Scripts/HidingSpot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingSpot : MonoBehaviour
+{
+    [Header("SETTINGS")]
+    [SerializeField] private Camera h_Cam;
+    [SerializeField] private Camera h_PlayerCamRef;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scenes/Rick/Scripts/PlayerController.cs
+++ Assets/Scenes/Rick/Scripts/PlayerController.cs
@@ -9,17 +9,12 @@ public class PlayerController : MonoBehaviour
 
     [Header("DEBUG")]
     [SerializeField] public Transform p_Transform;
-    [SerializeField] private Vector3 p_MoveDirection;
-    [SerializeField] private Rigidbody p_Rb;
     [SerializeField] private Camera p_Cam;
     [SerializeField] private bool p_isHiding;
-    [SerializeField] private float p_InputH;
-    [SerializeField] private float p_InputV;
 
     void Start()
     {
-        if (p_Rb == null) { p_Rb = GetComponent<Rigidbody>(); }
-        p_Rb.freezeRotation = true;
+
     }
 
     private void Update()
@@ -29,22 +24,16 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        PlayerMove();
+
     }
 
     private void PlayerInput()
     {
-        p_InputH = Input.GetAxisRaw("Horizontal");
-        p_InputV = Input.GetAxisRaw("Vertical");
+
     }
 
-    private void PlayerMove()
-    {
-        p_MoveDirection = p_Transform.forward * p_InputV + p_Transform.right * p_InputH;
-        p_Rb.AddForce(p_MoveDirection.normalized * p_MovementSpeed, ForceMode.Force);
-    }
 
-    private void PlayerHide()
+    public void PlayerHide()
     {
 
     }
